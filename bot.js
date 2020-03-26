bdb55c721e8077eadf590cdbe0cc2a547a579345
mhmudDev <44923654+mhmudDev@users.noreply.github.com>
2020-03-26 18:40:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "$"
-var adminprefix = '$'
+var prefix = "K"
+var adminprefix = 'K'
 
 
 //bc
@@ -23,7 +23,7 @@ client.on("message", message => {
 //bc online
 
 
-  var prefix = "$";
+  var prefix = "K";
 
   client.on("message", message => {
   
